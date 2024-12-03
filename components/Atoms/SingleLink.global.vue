b7331d26752fd9ac66ee80df348e364b2f9723b8
Dan Farrow <dan@zellement.com>
2024-12-03 21:06:41 +0000

    add alignment

--- components/Atoms/SingleLink.global.vue
+++ components/Atoms/SingleLink.global.vue
@@ -1,7 +1,8 @@
 <template>
     <nuxt-link
-        class="mr-auto flex items-center gap-4 self-start border-b border-current text-blue transition-colors hover:text-green-500"
+        class="flex items-center gap-4 border-b border-current text-blue transition-colors hover:text-green-500"
         :to="getUrl(content.link.cached_url ?? content.link.url)"
+        :class="alignText"
     >
         <span>{{ getTitle }}</span>
         <Icon name="ic:baseline-chevron-right" class="size-10" />
@@ -22,5 +23,16 @@ const getTitle: ComputedRef<string> = computed(() => {
         : props.content.link?.story?.name
           ? props.content.link?.story?.name
           : getUrl(props.content.link.cached_url)
+})
+
+const alignText: ComputedRef<string> = computed(() => {
+    switch (props.content.alignment) {
+        case 'centre':
+            return 'mx-auto'
+        case 'right':
+            return 'ml-auto'
+        default:
+            return 'mr-auto'
+    }
 })
 </script>
