b7a3e968edc03375dae395e740ff1f0e641a434e
Dan Farrow <dan@zellement.com>
2024-12-21 07:16:45 +0000

    revert

--- app.vue
+++ app.vue
@@ -75,6 +75,29 @@ watch(
 
 onMounted(async () => {
     await storyblokStore.fetchRequired()
+
+    if ($preview) {
+        const script = document.createElement('script')
+        script.src = 'https://app.storyblok.com/f/storyblok-v2-latest.js'
+        script.async = true
+        script.onload = () => {
+            const { StoryblokBridge, location } = window
+            const storyblokInstance = new StoryblokBridge({
+                resolveRelations: [
+                    'sectionCardBlock.cards',
+                    'sectionCardCarousel.cards',
+                    'sectionReviewBlock.reviews'
+                ],
+                preventClicks: true
+            })
+
+            storyblokInstance.on(['published', 'change'], () => {
+                // reload page if save or publish is clicked
+                location.reload()
+            })
+        }
+        document.head.appendChild(script)
+    }
 })
 
 useHead({
