62defa1b17506e937266cc40dd16f894b93d9ac3
Dan Farrow <dan@zellement.com>
2025-02-05 16:10:18 +0000

    fix

--- components/Sections/CardBlock.global.vue
+++ components/Sections/CardBlock.global.vue
@@ -1,16 +1,17 @@
 <template>
     <div class="container grid-layout container-px">
-        <single-card
-            v-for="card in content.cards"
-            :key="card.uuid"
-            :url="card.full_slug"
-            :name="card.name"
-            :img-data="{
-                url: card.content.hero?.[0]?.media.filename,
-                alt: card.content.hero?.[0]?.media.alt
-            }"
-            :overview="card.content.pageOverview"
-        />
+        <template v-for="card in content.cards" :key="card.uuid">
+            <single-card
+                v-if="card.content"
+                :url="card.full_slug"
+                :name="card.name"
+                :img-data="{
+                    url: card.content?.hero?.[0]?.media?.filename,
+                    alt: card.content?.hero?.[0]?.media?.alt
+                }"
+                :overview="card?.content?.pageOverview"
+            />
+        </template>
     </div>
 </template>
 
