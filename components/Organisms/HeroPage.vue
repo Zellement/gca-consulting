6f816f57afb45cde3a0c5d159da254f0573aec02
Dan Farrow <dan@zellement.com>
2025-02-05 16:30:36 +0000

    use nuxt picture

--- components/Organisms/HeroPage.vue
+++ components/Organisms/HeroPage.vue
@@ -16,12 +16,20 @@
                     :key="slide._uid"
                     :class="['embla__slide relative', 'w-full', slideClasses]"
                 >
-                    <single-picture
+                    <!-- <single-picture
                         class="h-full w-full object-cover"
                         :img-data="{
                             url: slide.media.filename,
                             alt: slide.media.alt ?? ''
                         }"
+                        sizes="336px md:700px xl:1200px 2xl:1600px max:1800px"
+                        loading="eager"
+                    /> -->
+                    <nuxt-picture
+                        :src="slide.media.filename"
+                        provider="storyblok"
+                        class="h-full w-full object-cover"
+                        :alt="slide.media.alt"
                         sizes="336px md:700px xl:1200px 2xl:1600px max:1800px"
                         loading="eager"
                     />
