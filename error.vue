0603a160c465057f6c6fcfa805b9fc6662c22ff0
Dan Farrow <dan@zellement.com>
2025-03-20 17:33:46 +0000

    contine

--- error.vue
+++ error.vue
@@ -1,9 +1,10 @@
 <template>
+    <site-header />
     <div class="page">
         <h1>Sorry, this page does not exist</h1>
         <nuxt-link to="/">Go back to the homepage</nuxt-link>
-        />
     </div>
+    <site-footer />
 </template>
 
 <script setup lang="ts">
