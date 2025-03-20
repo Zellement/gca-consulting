4872b30b18196916c2f450d3a4f644245efac811
Dan Farrow <dan@zellement.com>
2025-03-20 17:25:29 +0000

    add 404 and redirects

---
+++ error.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="page">
+        <h1>Sorry, this page does not exist</h1>
+        <nuxt-link to="/">Go back to the homepage</nuxt-link>
+        />
+    </div>
+</template>
+
+<script setup lang="ts">
+const storyblokStore = useStoryblokStore()
+
+const id = useId()
+
+const currentStory = computed(() => {
+    return storyblokStore.currentStory
+})
+
+const showHero: ComputedRef<boolean> = computed(() => {
+    return currentStory.value?.content?.showHero
+})
+
+definePageMeta({
+    title: 'Consulting Civil, Structural and Highway Engineers'
+})
+</script>
